ba3e9cde32ae9a295ffea277d02cda08e17a4141
Paul <mihali.paul123@gmail.com>
2021-04-29 19:04:09 +0100

    added macronutrients and vitamins pies

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,6 +1,8 @@
 import AddShoppingCartRoundedIcon from '@material-ui/icons/AddShoppingCartRounded';
 import axios from 'axios';
+
 import { useState, useEffect } from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
 import { Paper, Grid, Typography, makeStyles, Button, FormControl, OutlinedInput, withStyles } from '@material-ui/core';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 
@@ -17,10 +19,13 @@ const useStyles = makeStyles({
     height: 36,
   },
   addBtn: {
-    width: "100%",
+    width: '100%',
   },
   formGrid: {
     textAlign: 'center',
+  },
+  pie: {
+    margin: 'auto',
   },
 });
 
@@ -45,6 +50,7 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
+
 export default function ProductDetailPage() {
   const [product, setProduct] = useState({});
   const [quantity, setQuantity] = useState(1);
@@ -54,6 +60,25 @@ export default function ProductDetailPage() {
     'calcium (mg)', 'iron (mg)', 'magnesium (mg)', 'phosphorus (mg)', 'potassium (mg)', 'sodium (g)',
     'vitamin a (IU)', 'vitamin c (mg)', 'vitamin b1 (mg)', 'vitamin b2 (mg)', 'vitamin b3 (mg)',
     'vitamin b5 (mg)', 'vitamin b6 (mg)', 'vitamin e (mg)'];
+  const macronutrients = ['carbohydrates (g)', 'protein (g)', 'fat (g)', 'fiber (g)'];
+  const vitamins = ['vitamin a (IU)', 'vitamin c (mg)', 'vitamin b1 (mg)', 'vitamin b2 (mg)', 'vitamin b3 (mg)',
+    'vitamin b5 (mg)', 'vitamin b6 (mg)', 'vitamin e (mg)'];
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }, nutrientsList) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {percent > 0.01
+          ? `${nutrientsList[index].split(' (')[0]} (${(percent * 100).toFixed(0)}%)`
+          : ''}
+      </text>
+    );
+  };
 
   const handleQuntity = (e) => {
     setQuantity(e.target.value);
@@ -100,9 +125,9 @@ export default function ProductDetailPage() {
                   alignItems="center"
                 >
                   <Grid item xs={6} sm={6} md={3}>
-                    <Typography variant="h5">
+                    <Typography variant="h6">
                       £{product.price}
-                      <small>/{product.sell_per_unit ? "unit" : "200g"}</small>
+                      <small>/{product.sell_per_unit ? 'unit' : '200g'}</small>
                     </Typography>
                   </Grid>
 
@@ -158,19 +183,60 @@ export default function ProductDetailPage() {
 
                   <TableBody>
                     <StyledTableRow>
-                      <StyledTableCell align="right">energy</StyledTableCell>
-                      <StyledTableCell align="right">{product.kj} kJ ({product.kcal} kcal)</StyledTableCell>
+                      <StyledTableCell align="right">energy (kcal/kJ)</StyledTableCell>
+                      <StyledTableCell align="right">{product.kcal}/{product.kj}</StyledTableCell>
                     </StyledTableRow>
 
                     {nutrients.map((nutrient) => (
                       <StyledTableRow key={nutrient}>
                         <StyledTableCell align="right">{nutrient}</StyledTableCell>
-                        <StyledTableCell align="right">{product[nutrient].toFixed(3)}</StyledTableCell>
+                        <StyledTableCell align="right">
+                          {/* Dislay the value of the nutrient only if the product has that nutrient */}
+                          {product[nutrient] ? product[nutrient].toFixed(3) : '-'}
+                        </StyledTableCell>
                       </StyledTableRow>
                     ))}
                   </TableBody>
                 </Table>
               </TableContainer>
+            </Grid>
+
+            <Grid item xs={12} md={5}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Typography variant="h5" align="center">Macronutrients</Typography>
+                  <PieChart className={classes.pie} width={360} height={200}>
+                    <Pie
+                      data={macronutrients.map((nutrient) => ({ name: nutrient, value: product[nutrient] }))}
+                      labelLine={false}
+                      label={(pieData) => renderCustomizedLabel(pieData, macronutrients)}
+                      fill="#8884d8"
+                      dataKey="value"
+                    >
+                      {macronutrients.map((entry, i) => (
+                        <Cell key={i} fill={COLORS[i % COLORS.length]} />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography variant="h5" align="center">Vitamins</Typography>
+                  <PieChart className={classes.pie} width={360} height={200}>
+                    <Pie
+                      data={vitamins.map((nutrient) => ({ name: nutrient, value: product[nutrient] }))}
+                      labelLine={false}
+                      label={(pieData) => renderCustomizedLabel(pieData, vitamins)}
+                      fill="#8884d8"
+                      dataKey="value"
+                    >
+                      {vitamins.map((entry, i) => (
+                        <Cell key={i} fill={COLORS[i % COLORS.length]} />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
